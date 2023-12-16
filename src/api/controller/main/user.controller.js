7bf7155a3bcfbdd2d40bde482f646bb2ac6143a9
Muhammad Shurahbeel <tell2xb@gmail.com>
2023-12-16 19:50:06 +0500

    New Changes

--- src/api/controller/main/user.controller.js
+++ src/api/controller/main/user.controller.js
@@ -1,19 +1,29 @@
-const User = require('../../models/user.model')
+const User = require('../../models/user.model');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-exports.sigin = async (req, res, next) => {
+exports.sigin = async (req, res) => {
   try {
-    const { email, password } = req.body
-    console.log("All users");
-    console.log("email : ", email)
-    const user = await User.findOne({ email: email.toLowerCase() })
-    console.log("user: ", user)
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email.toLowerCase() });
+
     if (!user) {
-      return res.status(404).json({ message: "User don't exist with this email" })
+      return res.status(404).json({ message: "User doesn't exist with this email" });
     }
-    if (user.password !== password) return res.status(401).json({ message: "User password doesn't match" })
-    return res.status(200).json({ email: user.email, message: "You have login sucessfully" })
-  }
-  catch (err) {
+    console.log("password", password, user.password)
+
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatch) {
+      return res.status(401).json({ message: "User password doesn't match" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    return res.status(200).json({ token, email: user.email, message: "You have logged in successfully" });
+  } catch (err) {
+    console.error(err);
     res.json(err);
   }
-}
+};
